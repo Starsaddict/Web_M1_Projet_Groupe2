b78177c0abbe36d840acdd1c33e460b93593269e
walid00713 <wazecaarbon2@gmail.com>
2025-05-14 13:49:11 +0200

    ajout java doc

--- src/main/java/miage/groupe2/reseausocial/Controller/MainController.java
+++ src/main/java/miage/groupe2/reseausocial/Controller/MainController.java
@@ -1,12 +1,19 @@
 package miage.groupe2.reseausocial.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String homepage(){
+    @GetMapping("/home")
+    public String homepage() {
+        return "redirect:/home.html";
+    }
+
+    // Redirection de la racine vers la page home
+    @GetMapping("/")
+    public String redirectToHome() {
         return "redirect:/home.html";
     }
 }
