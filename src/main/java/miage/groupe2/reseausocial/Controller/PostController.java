f8fbcf25f5c960d953e5e3d79a66d82b613642d2
Starsaddict <18201122059zky@gmail.com>
2025-05-15 11:07:18 +0200

    repost

--- src/main/java/miage/groupe2/reseausocial/Controller/PostController.java
+++ src/main/java/miage/groupe2/reseausocial/Controller/PostController.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/post")
@@ -130,6 +132,34 @@ public class PostController {
 
         return "redirect:/user/" + user.getIdUti();
     }
+
+    @GetMapping("/repost")
+    public String repostPost(@RequestParam("id") Integer postId, HttpSession session, RedirectAttributes redirectAttributes) {
+        Utilisateur userInSession = (Utilisateur) session.getAttribute("user");
+        if (userInSession == null) {
+            return "redirect:/login";
+        }
+
+        Utilisateur user = utilisateurRepository.findByidUti(userInSession.getIdUti());
+
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if (optionalPost.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", "Post introuvable");
+            return "redirect:/post/list";
+        }
+
+        Post post = optionalPost.get();
+
+        List<Post> repostList = user.getPostsRepostes();
+        if (!repostList.contains(post)) {
+            repostList.add(post);
+            user.setPostsRepostes(repostList);
+            utilisateurRepository.save(user);
+        }
+
+        return "redirect:/user/" + user.getIdUti();
+    }
+
 
 
 
